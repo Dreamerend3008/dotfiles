9bf113d866aa0f14a269d03a480d19fb38f57740
harry <something@outlook.com>
2026-01-12 23:18:08 -0500

    Fix treesitter configuration back to original

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -125,7 +125,7 @@ require("lazy").setup({
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
+      require("nvim-treesitter").setup({
         ensure_installed = { 
           "lua", 
           "vim", 
